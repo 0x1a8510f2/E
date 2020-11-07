291b0bd45bc116bb8364dc7c80a39cd586edd3a2
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-11-07 02:16:06 +0000

    Initial setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/TR-SLimey/E
+
+go 1.15
+
+require maunium.net/go/mautrix v0.7.13
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"maunium.net/go/mautrix"
+)
+
+func main() {
+	println(mautrix.AuthTypePassword)
+}
