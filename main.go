b11ff09a93aa4b68db8b8876bb06466edb422c86
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-11-11 00:14:30 +0000

    Slightly improved formatting and added TODOs

--- main.go
+++ main.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"fmt"
+	//"flag"
+
+	//"github.com/spf13/viper"
 	"github.com/TR-SLimey/E/esockets"
-//	"maunium.net/go/mautrix"
+	//	"maunium.net/go/mautrix"
 )
 
 var (
 	// Basic info (static)
 	ProjectName = "E"
-	ProjectUrl = "https://github.com/TR-SLimey/E"
+	ProjectUrl  = "https://github.com/TR-SLimey/E"
 	// Incremented on release
 	ReleaseVersion = "pre-alpha"
 	// Filled at build time
-	VcsCommit = "unknown"
-	BuildTime = "unknown"
+	VcsCommit = "unknown_commit"
+	BuildTime = "unknown_build_time"
 	// Filled by init
-	VersionString = "unknown"
+	VersionString = "unknown_version_string"
 )
 
 func init() {
 	VersionString = fmt.Sprintf("%s %s %s [%s]", ProjectName, ReleaseVersion, BuildTime, VcsCommit)
+
+	// Config handling (TODO)
 }
 
 func main() {
