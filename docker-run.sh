2f8e26ba3213f13602dc1fbe4a0dcc5396b05123
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-11-12 23:32:10 +0000

    Minor docker-run.sh tweaks

--- docker-run.sh
+++ docker-run.sh
@@ -9,19 +9,19 @@ function fixperms {
 	chown -R $UID:$GID /data /opt/E
 }
 
-# Temporarily commented out for development
+# If the config was not found
 if [[ ! -f /data/config.yaml ]]; then
+	echo "[!] Could not find config.yaml"
 	cp /opt/E/example-config.yaml /data/config.yaml
-	echo "Could not find config.yaml"
-	echo "Copied default config file to /data/config.yaml"
-	echo "Please edit /data/config.yaml and re-start the container. This will generate a registration file."
+	echo "[#] Copied default config file to /data/config.yaml"
+	echo "[#] Please edit /data/config.yaml and re-start the container. This will generate a registration file to be placed on the homeserver."
 	exit
 fi
 
 #if [[ ! -f /data/registration.yaml ]]; then
 #	/usr/bin/E -g -c /data/config.yaml -r /data/registration.yaml
-#	echo "Could not find a regastration file so it has been generated."
-#	echo "Copy /data/registration.yaml to your homeserver."
+#	echo "[#] Could not find a registration file so it has been generated."
+#	echo "[#] Copy /data/registration.yaml to your homeserver."
 #	exit
 #fi
 
