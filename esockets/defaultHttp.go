16a68668954824c3e7ae42ed41d5930e21c4a7b4
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-11-13 17:27:49 +0000

    Minor cleanup

--- esockets/defaultHttp.go
+++ esockets/defaultHttp.go
@@ -1,12 +1,13 @@
 package esockets
 
 func init() {
+	// Create the esocket as a local variable
 	var esocket = Esocket{
 		ID: "defaultHttp",
+		onInit: func(es *Esocket) {
+			println(es.ID)
+		},
 	}
-	esocket.onInit = func(es *Esocket) {
-		println(es.ID)
-	}
-	// Register the esocket so that it can be used by E
+	// Register the esocket so that it can be listed and used
 	esocket.register()
 }
